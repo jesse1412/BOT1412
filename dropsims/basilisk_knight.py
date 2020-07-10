ae8a8d46137544f03d9a3bd589de9e3349299fc5
RoccoEE <47798035+RoccoEE@users.noreply.github.com>
2020-07-10 21:44:03 +0100

    Create basilisk_knight.py (#1)
    * Create basilisk_knight.py
    
    * Update basilisk_knight.py
    
    * Update basilisk_knight.py

---
+++ dropsims/basilisk_knight.py
@@ -0,0 +1,11 @@
+dropTables = [
+                {
+                    "Basilisk jaw (On task)": 1/1000
+                },
+                {
+                    "Basilisk head": 1/1000"
+                } 
+            ]
+trashDrops = ["Basilisk head"]
+
+description = "maf nooooo"
