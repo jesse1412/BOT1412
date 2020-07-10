1d322502f6baa8e59cc4b6f9108fb583f8d9b9ad
jesse1412 <jesseserrao@hotmail.co.uk>
2020-07-10 21:47:21 +0100

    Rename basilisk knights file.
    Tidy BK file.
    
    Update Basilisk Knight description.
    
    Remove random double quote.

--- dropsims/knights.py
+++ dropsims/knights.py
@@ -3,9 +3,10 @@ dropTables = [
                     "Basilisk jaw (On task)": 1/1000
                 },
                 {
-                    "Basilisk head": 1/1000"
+                    "Basilisk head": 1/1000
                 } 
             ]
+
 trashDrops = ["Basilisk head"]
 
-description = "maf nooooo"
+description = "Basilisk knights"
