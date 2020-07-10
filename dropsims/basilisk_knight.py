e7cdd6b42a5691304ab36db127fd03b7f81803d0
RoccoEE <47798035+RoccoEE@users.noreply.github.com>
2020-07-10 17:58:13 +0100

    Update basilisk_knight.py

--- dropsims/basilisk_knight.py
+++ dropsims/basilisk_knight.py
@@ -3,7 +3,7 @@ dropTables = [
                     "Basilisk jaw (On task)": 1/1000
                 },
                 {
-                                      "Basilisk head": 1/1000"
+                    "Basilisk head": 1/1000"
                 } 
             ]
 trashDrops = ["Basilisk head"]
