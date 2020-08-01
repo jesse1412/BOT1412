78258a430a6d3c84b574282e50f341df42a2c094
MeeranB <v9@live.co.uk>
2020-08-01 20:17:36 +0100

    add godsword shards to armadyl drop table

--- drop_simulations/arma.py
+++ drop_simulations/arma.py
@@ -3,7 +3,10 @@ drop_tables = [
                     "Armadyl helmet": 1/381,
                     "Armadyl chestplate": 1/381,
                     "Armadyl chainskirt": 1/381,
-                    "Armadyl hilt": 1/508
+                    "Armadyl hilt": 1/508,
+                    "Godsword shard 1": 1/762,
+                    "Godsword shard 2": 1/762,
+                    "Godsword shard 3": 1/762
                 },
                 {
                     "Pet kree'arra": 1/5000
